9fb15deb161fcd6466f1e0cdce6197d399bc59eb
maxDqin <max.qin1@gmail.com>
2022-03-16 15:56:01 +1100

    week02

---
+++ src/week02/SayHello.java
@@ -0,0 +1,15 @@
+package week02;
+
+public class SayHello {
+	public static void main(String[] args) {
+//		System.out.println("helloworld");
+//		System.out.println(5.0>=5);
+//		int c=7;
+//		c = c+1;
+//		System.out.println(c);
+		char a1 = 'a'; 
+		char a2 = (char)(a1+1); 
+		System.out.println(a1+1);
+		System.out.println(a2);
+	}
+}
