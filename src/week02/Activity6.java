3f8b166186010bf98316519a432a1fd40a2bb9f7
maxDqin <max.qin1@gmail.com>
2022-03-18 18:01:45 +1100

    switch case

---
+++ src/week02/Activity6.java
@@ -0,0 +1,16 @@
+package week02;
+
+public class Activity6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=32;i<=126;i++) {
+			char a = (char)i;
+			System.out.println(i + " -- " + a);
+		}
+		
+		int a = 7;
+		System.out.println(-a);
+	}
+
+}
