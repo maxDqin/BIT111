64b006f649e8251ad9e4c46b22ecb31c83895163
maxDqin <max.qin1@gmail.com>
2022-03-18 18:04:41 +1100

    switch-case

---
+++ src/week03/Exercise.java
@@ -0,0 +1,30 @@
+package week03;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Suppose we have a variable int a = 4; write codes to achieve:
+			if a>0, display “a is a positive number”
+			else if a = 0 display “a is 0”
+			else display “a is a negative number”
+		 */
+//		int a = -9;
+//		
+//		if(a>0) {
+//			System.out.println("a is a positive number");
+//		}else if(a==0) {
+//			System.out.println("a is 0");
+//		}else {
+//			System.out.println("a is a negative number");
+//		}
+//		System.out.println(0);
+//		System.out.println(1);
+//		System.out.println(2);
+		for(int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
