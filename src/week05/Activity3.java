bf2ef0d16199d42f72e6c673eaa38c248e3c45b2
maxDqin <max.qin1@gmail.com>
2022-04-02 11:59:26 +1100

    week 5 - A3

--- src/week05/Activity3.java
+++ src/week05/Activity3.java
@@ -8,11 +8,18 @@ public class Activity3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+
+		// Firstly, I accept the first number as the max one. (Assumption)
 		double max = sc.nextDouble();
-		for(int i=0;i<=9;i++) {
-			double num = sc.nextDouble();
+		
+		// After that, I accept the remaining 9 numbers to compare.
+		for(int i=0;i<9;i++) {
+			double num = sc.nextDouble(); // num is the input from console
 			max = Math.max(max, num);
 		}
+		
+		sc.close();
+		
 		System.out.println("The max one is: "+max);
 	}
 
